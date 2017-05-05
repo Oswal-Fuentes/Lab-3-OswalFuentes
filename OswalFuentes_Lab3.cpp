dabca3a0f404c69bcbe51d31aa478b8aff7859c4
Oswal <rene12215@gmail.com>
2017-05-05 16:53:53 -0600

    Otro avanze

--- OswalFuentes_Lab3.cpp
+++ OswalFuentes_Lab3.cpp
@@ -45,11 +45,48 @@ bool salir=false;
               }
         break;
       }//Fin ejercicio 1
-
-      case 2:{
+      case 2:{//Ejercicio 2
+	int dimension=0;
+	cout<<"Ingrese la dimension de la matriz: ";
+	cin>>dimension;
+	int matrix[dimension][dimension];
+	//-----LLENAR DE NUMEROS-----
+        for (int i = 0; i < dimension; i++) {
+          for (int j = 0; j < dimension; j++) {
+            matrix[i][j]=i+j;
+          }
+        }
+        //-----LLENAR DE NUMEROS-----
+	//---Imprimir---
+	for(int x=0;x<dimension;x++){
+           for(int y=0;y<dimension;y++){
+            cout<<matrix[x][y]<<"\t";
+          }
+            cout<<endl;
+         }
+	//---Imprimir---
+	//----Voltear----
+	int element=0,limite=dimension-1;
+        int matrix2[dimension][dimension];
+	for (int i = 0; i < dimension; i++) {
+          for (int j = 0; j < dimension; j++) {
+            element=matrix[i][j];
+	    matrix2[j][limite]=element;
+          }
+	limite--;
+        }
+	cout<<endl;
+	//---Imprimir---
+        for(int x=0;x<dimension;x++){
+          for(int y=0;y<dimension;y++){
+              cout<<matrix2[x][y]<<"\t";
+            }
+              cout<<endl;
+           }
+          //---Imprimir---
 
 	break;
-      }
+      }//Fin ejercicio 2
       case 3:{
 	salir=true;
 	break;
@@ -92,3 +129,4 @@ int suma_digitos(int numero) {
    }
    return suma;
 }
+
