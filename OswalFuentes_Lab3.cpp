26d89ff7daa53463160d849c470a5ae21b54f5ee
Oswal <rene12215@gmail.com>
2017-05-05 15:52:50 -0600

    ejercicio 1

--- OswalFuentes_Lab3.cpp
+++ OswalFuentes_Lab3.cpp
@@ -1,18 +1,51 @@
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <stdlib.h>
+
 
 using namespace std;
 
 //Funcion Menu
 int menu();
+//Funcion Suma digitos
+int suma_digitos(int);
 
 int main(){
 bool salir=false;
   while(!salir){
     switch(menu()){
-      case 1:{
-        
+      case 1:{//Ejercicio 1
+        int numero;
+        string binario = "";
+        cout << "Ingrese un numero: " << endl;
+        cin >> numero;
+        int recorrido=0;
+          while(recorrido<=numero){
+            int dividendo=0, residuo=0, divisor = 2;
+            dividendo = recorrido;
+              while(dividendo >= divisor){
+                residuo = dividendo % 2;
+                  if(residuo == 1)
+                    binario = "1" + binario;
+                  else
+                    binario = "0" + binario;
+                    dividendo = dividendo/divisor;
+              }
+              if(dividendo == 1)
+                binario = "1" + binario;
+              else
+                binario = "0" + binario;
+                int numero2 =0;
+	              numero2=atoi(binario.c_str());
+	              if(suma_digitos(numero2)%2==0){
+	                 cout<<recorrido<<" es un numero malvado"<<endl;
+                }
+                recorrido++;
+              }
         break;
-      }
+      }//Fin ejercicio 1
+
       case 2:{
 
 	break;
@@ -49,3 +82,13 @@ int menu(){
   return opcion;
 }
 
+int suma_digitos(int numero) {
+   int suma=0;
+   if (numero < 0)
+   numero=-numero;
+   while (numero!=0) {
+     suma+=numero%10;
+      numero=numero/10;
+   }
+   return suma;
+}
